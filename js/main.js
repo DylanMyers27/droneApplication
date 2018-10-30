8b1e4b4ab8253b9ac2384364109f783b086b84fb
Dylan Myers <DylanMyers@MacBook-Pro-66.local>
2018-10-30 15:48:03 -0400

    added promises to fetch data async

--- js/main.js
+++ js/main.js
@@ -1,61 +1,59 @@
-var actual_JSON = init();
+//fetch json
+var actual_JSON = function (){
+  fetch('data.json')
+    .then(data => data.json())
+    .then(json => actual_JSON = json)
+    .catch(err => console.log(err));
+}();
 
-function getDate(){
-  var d = new Date();
-  return d.toString();
-}
+//this function will reload the page every 10 seconds if it detects new data in json
+var previous = null;
+var current = null;
+setInterval(function getData(){
+     $.getJSON("data.json", function(json) {
+         current = JSON.stringify(json);
+         if (previous && current && previous !== current) {
+             console.log('refresh');
+             location.reload();
+         }
+         previous = current;
+         console.table(current);
+     });
+ },5000);
 
-function init() {
- loadJSON(function(response) {
-    actual_JSON = JSON.parse(response);
-
-    // console.log(actual_JSON);
- });
- return actual_JSON;
-}
-
-function loadJSON(callback) {
-    var xobj = new XMLHttpRequest();
-
-    xobj.overrideMimeType("application/json");
-    xobj.open('GET', "data.json", true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);
+//gets current date not used as of now
+ function getDate(){
+   var d = new Date();
+   return d.toString();
  }
 
-
+//this is to generate a table of all the results on button press
 function submitFunction() {
-    var i = 0;
-    actual_JSON = actual_JSON;
+var i = 0;
+var table = document.getElementById("myTable");
 
-while(i < actual_JSON.length){
-
-  var table = document.getElementById("myTable");
-  var row = table.insertRow(myTable.length);
-  if(actual_JSON[i].Latitude < 28.0  && actual_JSON[i].Longitude != 0.0){
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var cell4 = row.insertCell(3);
-    var cell5 = row.insertCell(4);
-    cell1.innerHTML = document.getElementById("fname").value;
-    cell2.innerHTML = actual_JSON[i].Datetime;
-    cell3.innerHTML = actual_JSON[i].Latitude;
-    cell4.innerHTML = actual_JSON[i].Longitude;
-    // cell6.innerHTML = actual_JSON[i].Nitrate;
-    }
+  while(i < actual_JSON.length){
+    var row = table.insertRow(myTable.length);
+      if(actual_JSON[i].Latitude < 28.0  && actual_JSON[i].Longitude != 0.0){
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var cell4 = row.insertCell(3);
+        var cell5 = row.insertCell(4);
+        cell1.textContent = document.getElementById("fname").value;
+        cell2.textContent = actual_JSON[i].Datetime;
+        cell3.textContent = actual_JSON[i].Latitude;
+        cell4.textContent = actual_JSON[i].Longitude;
+        cell5.innerHTML = actual_JSON[i].Altitude;
+      }
     i++;
   }
 }
 
 
+
 //////////////////////////////////////////////
-function exportTableToCSV() {
+function exportTableToCSV() { //does what function name says
 
     var csv = [];
 
@@ -104,15 +102,13 @@ function downloadCSV(csv, filename) {
 
 
 
-function initMap() {
-
-  // actual_JSON = init();
+function initMap() { // initializes the google map
 
   var center = {lat: Number(actual_JSON[0].Latitude), lng: Number(actual_JSON[0].Longitude)};
 
   var map = new google.maps.Map(document.getElementById('map'), {
     center: center,
-    zoom: 17,
+    zoom: 18,
     mapTypeId: google.maps.MapTypeId.HYBRID
   });
   var infowindow =  new google.maps.InfoWindow;
@@ -124,7 +120,7 @@ function initMap() {
     });
   google.maps.event.addListener(marker, 'click', (function (marker, count) {
       return function () {
-        infowindow.setContent("Altitude: " + actual_JSON[count].Altitude);
+        infowindow.setContent("Nitrate Level: " + actual_JSON[count].Altitude + " mg/L");
         infowindow.open(map, marker);
       }
     })(marker, count));
@@ -132,9 +128,11 @@ function initMap() {
 }
 
 
+
+
 jQuery(document).ready(function() { //this fuction is for the back to top button
     var offset = 220;
-    var duration = 500;
+    var duration = 100;
     jQuery(window).scroll(function() {
         if (jQuery(this).scrollTop() > offset) {
             jQuery('.back-to-top').fadeIn(duration);
@@ -148,4 +146,55 @@ jQuery(document).ready(function() { //this fuction is for the back to top button
         jQuery('html, body').animate({scrollTop: 0}, duration);
         return false;
     })
+});
+
+// had loading issues so this fetches data seperately
+$.get("data.json", function (data) { //this is the dynamic CanvasJS chart
+
+var dps = []; // dataPoints
+var chart = new CanvasJS.Chart("chartContainer", {
+    title :{
+        text: "Nitrate Concentrations"
+    },
+    axisY: {
+        includeZero: false,
+        title: "Nitrate Levels",
+    },
+    axisX:{
+        title: "Number of Samples"
+    },
+    data: [{
+        type: "line",
+        dataPoints: dps,
+    }]
+});
+
+
+var xVal = 1;
+var yVal = 5.0;
+var updateInterval = 2500;
+var dataLength = 5; // number of dataPoints visible at any point
+
+var updateChart = function (count) {
+
+    count = count || 1;
+
+    for (var j = 0; j < count; j++) {
+        yVal = Number(actual_JSON[j].Altitude);
+        dps.push({
+            x: xVal,
+            y: yVal
+        });
+        xVal++;
+    }
+
+    if (dps.length < dataLength) {
+        dps.shift();
+    }
+
+    chart.render();
+};
+
+updateChart(dataLength);
+setInterval(function(){updateChart()}, updateInterval);
 });
